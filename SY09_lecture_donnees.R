973f49f51d8cec3dce22c0b61a47d9bd3659b649
Claire LE RAY <claire.le-ray@etu.utc.fr>
2019-05-05 22:27:50 +0200

    Sommaire/To-do list

--- SY09_lecture_donnees.R
+++ SY09_lecture_donnees.R
@@ -1,2 +1,6 @@
-transfers <- read.csv("Projet/top-250-football-transfers-from-2000-to-2018/top250-00-19.csv")
+transfers <- read.csv("top-250-football-transfers-from-2000-to-2018/top250-00-19.csv")
 summary(transfers)
+
+# A faire
+   # Factorisation League_from et League_to (doublons, orthographe, langue)
+   # Que faire des NA dans Market Value ?
