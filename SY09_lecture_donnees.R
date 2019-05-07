5dd38121d6e6d28ccf8c1caf29db4f09759d3b62
cbrizard <clement.brizard@etu.utc.fr>
2019-05-07 18:16:15 +0200

    Ajout README

--- SY09_lecture_donnees.R
+++ SY09_lecture_donnees.R
@@ -1,5 +1,22 @@
-transfers <- read.csv("top-250-football-transfers-from-2000-to-2018/top250-00-19.csv")
+transfers <- read.csv("top-250-football-transfers-from-2000-to-2018/top250-00-19.csv", header = T)
 summary(transfers)
+
+colnames(transfers)
+
+# Combien de variables ?
+ncol(transfers)
+
+# Combien de transferts ?
+nrow(transfers)
+length(levels(transfers$Season))
+as.data.frame(table(transfers$Season))
+
+help(table)
+
+
+
+
+
 
 # A faire
    # Factorisation League_from et League_to (doublons, orthographe, langue)
